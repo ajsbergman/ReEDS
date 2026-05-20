1b54ff2f34974690bc11d640355b6589d2b3dd38
pesap <pesap@users.noreply.github.com>
2026-05-19 21:05:40 -0600

    fix(gams): pass license file to gams cli

--- tests/framework_comparison/run_gams.sh
+++ tests/framework_comparison/run_gams.sh
@@ -6,4 +6,15 @@ gms_path="${2:?gms path required}"
 lst_path="${3:?lst path required}"
 curdir="${4:?curdir required}"
 
-exec "${gams_exe}" "${gms_path}" lo=0 "o=${lst_path}" "curdir=${curdir}"
+license_arg=""
+if [[ -n "${GAMS_LICENSE_FILE:-}" ]]; then
+  license_arg="license=${GAMS_LICENSE_FILE}"
+elif [[ -n "${GAMS_LICENSE:-}" ]]; then
+  license_arg="license=${GAMS_LICENSE}"
+fi
+
+if [[ -n "${license_arg}" ]]; then
+  exec "${gams_exe}" "${gms_path}" "${license_arg}" lo=0 "o=${lst_path}" "curdir=${curdir}"
+else
+  exec "${gams_exe}" "${gms_path}" lo=0 "o=${lst_path}" "curdir=${curdir}"
+fi
