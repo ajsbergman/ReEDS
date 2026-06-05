40493807fbe9aa3b10986f799a14ab1aaa35e527
kristinaavery <kristina.pitman@nrel.gov>
2026-06-05 11:53:30 -0600

    srun template

--- reeds/hpc/srun_template.sh
+++ reeds/hpc/srun_template.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --account=[your HPC allocation]
+#SBATCH --account=finitoreeds
 #SBATCH --time=2-00:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --mail-user=[your email address]
+#SBATCH --mail-user=kavery@nlr.gov
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mem=246000        # RAM in MB; up to 246000 for normal or 2000000 for bigmem on kestrel
 # add >>> #SBATCH --qos=high <<< above for quicker launch at double AU cost
