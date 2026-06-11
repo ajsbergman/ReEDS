35820e5c6ad3fe0f5412fe574c6684575c3d6170
An Pham <An.Pham@nlr.gov>
2026-06-11 17:01:54 -0600

    clean up

--- reeds/input_processing/copy_files.py
+++ reeds/input_processing/copy_files.py
@@ -1224,7 +1224,7 @@ def write_miscellaneous_files(
         os.path.join(reeds_path,'inputs','employment','employment_factor_inter_transmission.csv'),
         index_col=0)
     ef_trans = ef_trans[ef_trans.index == sw['GSw_EmploymentFactor']].T.round(8).to_csv(
-        os.path.join(inputs_case,'employment_factor_inter_transmission.csv'),index=False)
+        os.path.join(inputs_case,'employment_factor_inter_transmission.csv'),header=False)
     
     # Add this_year to years_until_endogenous to generate the tech-specific firstyear.csv file
     scalars = reeds.io.get_scalars(full=True)
